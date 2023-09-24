e6f546ba21e600f781573ed811228bb6176708ec
Yuriy Petrashev <yurapetr@yahoo.com>
2023-09-24 13:29:38 +0600

    FAQ Page exercise

--- main.go
+++ main.go
@@ -15,6 +15,27 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email ne at <a href=\"mailto:my@email.net\">my@email.net</a>.")
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, `<h1>FAQ Page</h1>
+	<ul>
+  <li>
+    <b>Is there a free version?</b>
+    Yes! We offer a free trial for 30 days on any paid plans.
+  </li>
+  <li>
+    <b>What are your support hours?</b>
+    We have support staff answering emails 24/7, though response
+    times may be a bit slower on weekends.
+  </li>
+  <li>
+    <b>How do I contact support?</b>
+    Email us - <a href="mailto:support@lenslocked.com">support@lenslocked.com</a>
+  </li>
+</ul>
+	`)
+}
+
 // func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
@@ -34,6 +55,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
 	}
