977bec3dd749292ec28131a2198df2a93f88590f
Yuriy Petrashev <yurapetr@yahoo.com>
2023-09-24 13:13:23 +0600

    Exploring HAndler Conversion

--- main.go
+++ main.go
@@ -15,20 +15,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Contact page</h1><p>To get in touch, email ne at <a href=\"mailto:my@email.net\">my@email.net</a>.")
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	default:
-		http.Error(w, "Page not found", http.StatusNotFound)
-	}
-}
-
-// type Router struct{}
-
-// func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	switch r.URL.Path {
 // 	case "/":
 // 		homeHandler(w, r)
@@ -39,7 +26,21 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 // 	}
 // }
 
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.Error(w, "Page not found", http.StatusNotFound)
+	}
+}
+
 func main() {
+	var router Router
 	fmt.Println("Starting server...")
-	http.ListenAndServe(":3000", http.HandlerFunc(pathHandler))
+	http.ListenAndServe(":3000", router)
 }
