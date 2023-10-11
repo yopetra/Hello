260e3891cc017875bf96d86294d8af1115f3e451
Yuriy Petrashev <yurapetr@yahoo.com>
2023-10-11 09:37:12 +0600

    Clean code

--- main.go
+++ main.go
@@ -10,31 +10,6 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-// func executeTemplate(w http.ResponseWriter, filepath string) {
-// t, err := views.Parse(filepath)
-// if err != nil {
-// log.Printf("parsing templat: %v, err")
-// http.Error(w, "There was an error parsing the template.",
-// http.StatusInternalServerError)
-// return
-// }
-// t.Execute(w, nil)
-// }
-
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// tplPath := filepath.Join("templates", "home.gohtml")
-// executeTemplate(w, tplPath)
-// }
-
-// func contactHandler(w http.ResponseWriter, r *http.Request) {
-// tplPath := filepath.Join("templates", "contact.gohtml")
-// executeTemplate(w, tplPath)
-// }
-
-// func faqHandler(w http.ResponseWriter, r *http.Request) {
-// executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-// }
-
 func main() {
 	r := chi.NewRouter()
 	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
