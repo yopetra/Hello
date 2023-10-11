7646f9b8287ab76c839ab1557c8b89d3bf26eada
Yuriy Petrashev <yurapetr@yahoo.com>
2023-10-11 09:35:23 +0600

    Validating Templates at Startup

--- main.go
+++ main.go
@@ -2,44 +2,60 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
 
+	"example.com/mod/controllers"
 	"example.com/mod/views"
 	"github.com/go-chi/chi/v5"
 )
 
-func executeTemplate(w http.ResponseWriter, filepath string) {
-	t, err := views.Parse(filepath)
-	if err != nil {
-		log.Printf("parsing templat: %v, err")
-		http.Error(w, "There was an error parsing the template.",
-			http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
+// func executeTemplate(w http.ResponseWriter, filepath string) {
+// t, err := views.Parse(filepath)
+// if err != nil {
+// log.Printf("parsing templat: %v, err")
+// http.Error(w, "There was an error parsing the template.",
+// http.StatusInternalServerError)
+// return
+// }
+// t.Execute(w, nil)
+// }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "home.gohtml")
-	executeTemplate(w, tplPath)
-}
+// func homeHandler(w http.ResponseWriter, r *http.Request) {
+// tplPath := filepath.Join("templates", "home.gohtml")
+// executeTemplate(w, tplPath)
+// }
 
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	tplPath := filepath.Join("templates", "contact.gohtml")
-	executeTemplate(w, tplPath)
-}
+// func contactHandler(w http.ResponseWriter, r *http.Request) {
+// tplPath := filepath.Join("templates", "contact.gohtml")
+// executeTemplate(w, tplPath)
+// }
 
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
+// func faqHandler(w http.ResponseWriter, r *http.Request) {
+// executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
+// }
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", homeHandler)
-	r.Get("/contact", contactHandler)
-	r.Get("/faq", faqHandler)
+	tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	r.Get("/", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/contact", controllers.StaticHandler(tpl))
+
+	tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+
+	r.Get("/faq", controllers.StaticHandler(tpl))
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
 	})
