f586701b77f28253a89f72258e7153cb911eea69
Yuriy Petrashev <yurapetr@yahoo.com>
2023-10-15 09:38:25 +0600

    Embedding templates files

--- main.go
+++ main.go
@@ -3,27 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path/filepath"
 
 	"example.com/mod/controllers"
+	"example.com/mod/templates"
 	"example.com/mod/views"
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
 	r := chi.NewRouter()
-	tpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
-	r.Get("/", controllers.StaticHandler(tpl))
+	r.Get("/", controllers.StaticHandler(
+		views.Must(views.ParseFS(templates.FS, "home.gohtml"))))
 
-	// Or we can use inline for everything
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "contact.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
 	r.Get("/faq", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "faq.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.Get("/welcome", controllers.StaticHandler(
-		views.Must(views.Parse(filepath.Join("templates", "welcome.gohtml")))))
+		views.Must(views.ParseFS(templates.FS, "welcome.gohtml"))))
 
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Page not found", http.StatusNotFound)
