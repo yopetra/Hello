91e971aeea7b5d668b58a99dcb57aa2e1b96becb
Yuriy Petrashev <yurapetr@yahoo.com>
2023-10-18 09:03:34 +0600

    Use dinamic data in FAQ page

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	r.Get("/contact", controllers.StaticHandler(
 		views.Must(views.ParseFS(templates.FS, "contact.gohtml"))))
 
-	r.Get("/faq", controllers.StaticHandler(
+	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(templates.FS, "faq.gohtml"))))
 
 	r.Get("/welcome", controllers.StaticHandler(
