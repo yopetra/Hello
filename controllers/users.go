d39308d64ba0e601e92b7649e900476be5e051a3
Yuriy Petrashev <yurapetr@yahoo.com>
2023-11-23 08:29:21 +0600

    decouple with interfaces

--- controllers/users.go
+++ controllers/users.go
@@ -2,17 +2,14 @@ package controllers
 
 import (
 	"net/http"
-
-	"example.com/mod/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	// We need a view to render
 	u.Templates.New.Execute(w, nil)
 }
