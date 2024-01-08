a099e9d4945e1dd9fc6fa72386a1d059ee3d336e
Yuriy Petrashev <yurapetr@yahoo.com>
2024-01-08 09:25:29 +0600

    Added docked config

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -7,8 +7,29 @@ import (
 	_ "github.com/jackc/pgx/v4/stdlib"
 )
 
+type PostgressConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	SSLMode  string
+}
+
+func (cfg PostgressConfig) String() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
+}
+
 func main() {
-	db, err := sql.Open("pgx", "host=localhost port=5433 user=baloo password=junglebook dbname=lenslocked sslmode=disable")
+	cfg := PostgressConfig{
+		Host:     "localhost",
+		Port:     "5433",
+		User:     "baloo",
+		Password: "junglebook",
+		Database: "lenslocked",
+		SSLMode:  "disable",
+	}
+	db, err := sql.Open("pgx", cfg.String())
 	if err != nil {
 		panic(err)
 	}
