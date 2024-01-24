20cf6ee8c81d37504c0fd177d9e16e16420b8b6c
Yuriy Petrashev <yurapetr@yahoo.com>
2024-01-24 09:43:25 +0600

    cli_compare_func

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -29,5 +29,10 @@ func hash(password string) {
 }
 
 func compare(password, hash string) {
-	fmt.Printf("TODO: Compare the password %q with the hash %q\n", password, hash)
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	if err != nil {
+		fmt.Printf("Password is invalid: %v\n", password)
+		return
+	}
+	fmt.Println("Password is correct!")
 }
