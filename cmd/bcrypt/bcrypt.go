4c503739fa0009a76298dc656a9a313bc74382e0
Yuriy Petrashev <yurapetr@yahoo.com>
2024-01-24 09:32:27 +0600

    hash_func

--- cmd/bcrypt/bcrypt.go
+++ cmd/bcrypt/bcrypt.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
@@ -17,7 +19,13 @@ func main() {
 }
 
 func hash(password string) {
-	fmt.Printf("TODO: Hash the password %q\n", password)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		fmt.Printf("error hashing: %v\n", err)
+		return
+	}
+	hash := string(hashedBytes)
+	fmt.Println(hash)
 }
 
 func compare(password, hash string) {
